dc31a0d962dc9b89ba4d80d4a177c47257e699eb
Chris <lilunlogic@163.com>
2022-04-11 22:06:24 +0800

    add springboot Retryable demo

--- src/main/java/com/chris/cloud/Cloud2022Application.java
+++ src/main/java/com/chris/cloud/Cloud2022Application.java
@@ -2,8 +2,10 @@ package com.chris.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.retry.annotation.EnableRetry;
 
 @SpringBootApplication
+@EnableRetry
 public class Cloud2022Application {
 
     public static void main(String[] args) {
