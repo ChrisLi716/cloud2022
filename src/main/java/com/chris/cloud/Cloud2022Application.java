b649b56f59127fc624d11b2e182128692d47063a
chris <lilunlogic@163.com>
2022-11-05 19:28:08 +0800

    prometheus+grafana实操

--- src/main/java/com/chris/cloud/Cloud2022Application.java
+++ src/main/java/com/chris/cloud/Cloud2022Application.java
@@ -1,7 +1,11 @@
 package com.chris.cloud;
 
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.retry.annotation.EnableRetry;
 
 @SpringBootApplication
@@ -10,6 +14,11 @@ public class Cloud2022Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Cloud2022Application.class, args);
+    }
+
+    @Bean
+    MeterRegistryCustomizer<MeterRegistry> config(@Value("${spring.application.name}") String applicationName) {
+        return (registry) -> registry.config().commonTags("fuck", applicationName);
     }
 
 }
